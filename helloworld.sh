4d54398e95f3328baf73c27909dca9b405c63968
root <root@ip-172-31-93-206.ec2.internal>
2020-05-09 12:11:55 +0000

    My 6th Commit

--- helloworld.sh
+++ helloworld.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 PASSWORD=$(date +%s)
 echo "${PASSWORD}"
-<<<<<<< HEAD
+
 echo "Hi All"
 echo "Have a good Day"
-echo "{Hello jenkins}"
-echo "{Hello All}"
+
+echo "Hello jenkins"
+echo "Hello All"
+echo "12345"
